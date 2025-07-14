b0cd46fae723bdbe6b000760f86e72e796a1156d
rzh <peter.rao@derbysoft.net>
2025-07-14 20:23:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['.mjs', '.wasm'],
-  server: {
-    fs: {
-      allow: ['/Users/rzh/Documents/GitHub/rmbg-tool', 'src/wasm']
+export default defineConfig(({ command }) => {
+  console.log('command', command);
+  return {
+    plugins: [react(), copy({
+      targets: [{
+        src: 'src/wasm/**/*',
+        dest: 'dist/assets'
+      }],
+      hook: 'writeBundle'
+    })],
+    assetsInclude: ['.mjs', '.wasm'],
+    server: {
+      fs: {
+        allow: ['/Users/rzh/Documents/GitHub/rmbg-tool', 'src/wasm']
+      }
     }
   }
 })
